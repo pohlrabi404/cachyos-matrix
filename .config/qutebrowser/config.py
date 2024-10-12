cae415d0c85a7064daaca367153cb6ba3b8bac96
Pohlrabi <pohlrabi404@gmail.com>
2024-10-12 13:18:53 +0900

    i dont like spotify

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -26,6 +26,23 @@ c.colors.tabs.selected.odd.bg = '#00b000'
 c.colors.tabs.even.bg = '#000000'
 c.colors.tabs.odd.bg = '#000000'
 
+c.colors.contextmenu.menu.bg = '#000000'
+c.colors.contextmenu.menu.fg = '#00b000'
+
+c.colors.completion.item.selected.bg = '#00b000'
+c.colors.completion.item.selected.border.top = '#00b000'
+c.colors.completion.item.selected.border.bottom = '#00b000'
+c.colors.completion.match.fg = '#50ff50'
+
+c.colors.completion.category.bg = '#000000'
+c.colors.completion.category.border.top = '#00b000'
+c.colors.completion.category.border.bottom = '#00b000'
+c.colors.completion.category.fg = '#00b000'
+
+c.colors.completion.even.bg = '#000000'
+c.colors.completion.odd.bg = '#000000'
+c.colors.completion.fg = '#00b000'
+
 c.colors.hints.fg = '#009000'
 c.colors.hints.match.fg = '#00ff00'
 c.colors.hints.bg = '#000000'
@@ -38,5 +55,9 @@ c.hints.padding = {
 c.hints.radius = 0
 c.hints.border = '1px solid #00f000'
 
+c.url.start_pages = [
+        'https://discord.com/channels/@me',
+        'https://start.duckduckgo.com/?q=DuckDuckGo+AI+Chat&ia=chat&duckai=1'
+        ]
 
 config.bind('<Escape>', 'mode-leave ;; jseval -q document.activeElement.blur()', mode='insert')

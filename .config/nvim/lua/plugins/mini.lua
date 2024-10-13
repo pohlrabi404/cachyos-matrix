f8a7c792313fc0221d16fa14aada0ace88009abb
Pohlrabi <pohlrabi404@gmail.com>
2024-10-14 03:52:29 +0900

    setup mpv for performance

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -4,7 +4,11 @@ return {
       require('mini.ai').setup() 
       require('mini.icons').setup()
       require('mini.completion').setup()
-      require('mini.jump2d').setup()
+      require('mini.jump2d').setup({
+        mappings = {
+          start_jumping = ';;'
+        }
+      })
       require('mini.notify').setup()
       require('mini.pairs').setup()
       require('mini.sessions').setup({

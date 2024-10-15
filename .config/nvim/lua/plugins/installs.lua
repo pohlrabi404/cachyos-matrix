b6b825860d0b43afee9886afd5eeb6e247a110ce
Pohlrabi <pohlrabi404@gmail.com>
2024-10-16 01:35:32 +0900

    delete unimportant files

--- .config/nvim/lua/plugins/installs.lua
+++ .config/nvim/lua/plugins/installs.lua
@@ -1,15 +1,24 @@
 return {
   -- Telescope
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make',
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
   },
-  { 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
   {
     "nvim-telescope/telescope-file-browser.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
   },
 
   -- Mini
-  { 'echasnovski/mini.nvim', version = false },
+  {
+    'echasnovski/mini.nvim',
+    version = false
+  },
 
   -- Appearance 
   {

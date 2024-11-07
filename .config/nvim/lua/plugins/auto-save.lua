e269b301d1a51df488a3c8b96b45e96f2dfa94cf
Pohlrabi <pohlrabi404@gmail.com>
2024-11-08 08:42:44 +0900

    change config

--- .config/nvim/lua/plugins/auto-save.lua
+++ .config/nvim/lua/plugins/auto-save.lua
@@ -1,1 +1,2 @@
 require("auto-save").setup({})
+
--- .config/nvim/lua/plugins/test.lua
+++
@@ -1,7 +0,0 @@
-function set(m, k, f, st)
-  settings = { silent = true, noremap = false } 
-  if st ~= nil then
-    for key, value in pairs(st) do settings[key] = value end
-  end
-  vim.keymap.set(m, k, f, st)
-end

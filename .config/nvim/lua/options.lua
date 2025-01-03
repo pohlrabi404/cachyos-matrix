63b39adffd63923367f5900b400b3183438456fd
Pohlrabi <pohlrabi404@gmail.com>
2025-01-03 19:59:40 +0900

    move to init func

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -72,5 +72,3 @@ local is_windows = vim.fn.has("win32") ~= 0
 local sep = is_windows and "\\" or "/"
 local delim = is_windows and ";" or ":"
 vim.env.PATH = table.concat({ vim.fn.stdpath("data"), "mason", "bin" }, sep) .. delim .. vim.env.PATH
-
-vim.g.python3_host_prog = vim.fn.expand("~/.virtualenvs/neovim/bin/python3")
